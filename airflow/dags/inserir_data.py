ba679c442b576af8d126b4eb3428b1452fbd9254
xpcosmos <mikeias.d.s.o@gmail.com>
2025-03-15 11:54:34 -0300

    refatoramento: removendo códigos que confirmam se dados foram baixados

--- airflow/dags/inserir_data.py
+++ airflow/dags/inserir_data.py
@@ -1,12 +1,10 @@
-import os
+# import os
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+# from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 
 AIRFLOW_HOME = "/opt/airflow"
-
-
 
 YELLOW_TIP_URL = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-01.parquet'
 LOOKUP_TABLE = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv'
@@ -15,12 +13,6 @@ dag =  DAG(dag_id="inserir_data_gcp",
         description="Baixa dados de site",
         catchup=False,
         start_date=datetime(2024,10,26))
-
-echoo = BashOperator(
-        task_id='echoo',
-        bash_command='echo "iniciando working"',
-        dag=dag
-    )
 
 # TODO: Adicionar informação de sucesso utilizando-se do XCom em vez de ls_active
 # TODO:
@@ -36,10 +28,4 @@ download_lookup_table = BashOperator(
         dag=dag
     )
 
-ls_active = PythonOperator(
-        task_id='ls_active',
-        python_callable=os.listdir,
-        dag=dag
-    )
-
-echoo >> [download_lookup_table, download_yellow_tip] >> ls_active
+[download_lookup_table, download_yellow_tip]
